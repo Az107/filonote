cefe0f8751507f289bf2b145605a483f5932e5a0
Alberto Ruiz <albertzruiz1@gmail.com>
2021-02-11 10:20:17 +0100

    fix: :bug: add missing assets to service worker
    add assets , css and scripts to service worker

--- pwabuilder-sw.js
+++ pwabuilder-sw.js
@@ -12,10 +12,19 @@ self.addEventListener("message", (event) => {
   }
 });
 
+const assets = [
+  "/", 
+  "index.html",
+  "Scripts/engine.js",
+  "Scripts/angular.min.js",
+  "Scripts/jquery-3.5.1.min.js",
+  "Styles/tailwind.min.css"
+];
+
 self.addEventListener('install', async (event) => {
   event.waitUntil(
     caches.open(CACHE)
-      .then((cache) => cache.add(offlineFallbackPage))
+      .then((cache) => cache.addAll(assets))
   );
 });
 
