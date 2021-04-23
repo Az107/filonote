dbf7bf4cdea5ec1543fec6ce19b6acfdd3ff0a87
Alberto Ruiz <albertzruiz1@outlook.es>
2021-04-23 22:34:56 +0200

    feat: :sparkles: add delNote to api
    allow delete notes from api

---
+++ api/delNote.js
@@ -0,0 +1,31 @@
+const {MongoClient, ObjectID} = require("mongodb");
+const uri = process.env.MONGOURI;
+const client = MongoClient(uri);
+
+
+
+exports.handler = async (req) => {
+    let res = {
+        statusCode: 200,
+        body: ""
+    }
+
+    await client.connect();
+    let database = await client.db('Filonote');
+    let notes = await database.collection("Notes");
+    let tokens = await database.collection('Tokens');
+
+    let args = JSON.parse(req.body);
+    let Token = await tokens.findOne({token: args.Token});
+    console.log(args.Token);
+    args.Notes.forEach(note => {
+        notes.updateOne({userId: Token.userId},{$pull: {Notes: note}});
+    });
+
+
+    return res;
+
+    
+    
+
+}
